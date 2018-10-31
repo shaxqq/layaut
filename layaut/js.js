7ef0f9738151244e2b426a3b0a1f15d49e4af43e
shaxqq <39559342+shaxqq@users.noreply.github.com>
2018-10-31 14:03:05 +0200

    Add files via upload

---
+++ layaut/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.banner__scroll').slick({
+        dots: true,
+        arrows: true,
+        nextArrow: '<div class="arrow__right"></div>',
+        prevArrow: '<div class="arrow__left"></div>'
+    });
+  });
+      
